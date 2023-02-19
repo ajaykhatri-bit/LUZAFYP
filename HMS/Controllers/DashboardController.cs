9976004b745df9140eb0f6e94437c30808c5c2de
teamnextnepal <91668631+teamnextnepal@users.noreply.github.com>
2023-02-19 15:30:52 +0545

    initial set up

---
+++ HMS/Controllers/DashboardController.cs
@@ -0,0 +1,34 @@
+﻿using HMS.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HMS.Controllers
+{
+    [Authorize]
+    public class DashboardController : Controller
+    {
+        private readonly ILogger<DashboardController> _logger;
+
+        public DashboardController(ILogger<DashboardController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HMS/Data/ApplicationDbContex.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HMS.Data
+{
+    public class ApplicationDbContex : IdentityDbContext
+    {
+        public ApplicationDbContex(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
